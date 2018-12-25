f152061443cc6471b484097d235d0e28954886cd
Peter Signore <psignore@gmail.com>
2018-12-25 15:39:09 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Peter Signore on 25/12/2018.
+//  Copyright © 2018 Peter Signore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
